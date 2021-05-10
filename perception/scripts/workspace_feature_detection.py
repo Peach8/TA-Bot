d63a2b3eb4e92dbcd2110e8f8055ea515093a9fc
Peach8 <btpeterson08@gmail.com>
2021-05-10 13:53:44 -0700

    Implement green color and contour detection for base fiducial; TODO: add error handling for no green detected

--- perception/scripts/workspace_feature_detection.py
+++ perception/scripts/workspace_feature_detection.py
@@ -15,7 +15,33 @@ def callback(data):
 	# make copy for post-processing
 	img = current_frame.copy()
 
-	## detect robot base and corner of paper
+	## Detect robot base via green fiducial sticker
+	# - define hsv range corresponding to color green
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	green_mask = cv2.inRange(hsv, (40, 30, 30), (70, 255, 255))
+	
+	# - generate image that is black except for detected green regions
+	imask = green_mask>0
+	green = np.zeros_like(img, np.uint8)
+	green[imask] = img[imask]
+	
+	# - use largest detected contour to define circle around green region
+	gray = cv2.cvtColor(green, cv2.COLOR_BGR2GRAY) # contour detection requries grayscale
+	contours,_ = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	areas = [cv2.contourArea(c) for c in contours]
+	sorted_areas = np.sort(areas)
+	cnt = contours[areas.index(sorted_areas[-1])] # save the biggest contour
+
+	# - build circle based on largest detected contour and minimium enclosing circle
+	(x,y),radius = cv2.minEnclosingCircle(cnt)
+	center = (int(x), int(y))
+	radius = int(radius)
+	# - draw result on top of original raw image
+	cv2.circle(img, center, radius, (0,0,255), 2)
+
+	## Detect top-right paper corner via Harris
+	
+
 
 	cv2.imshow('feature_detection', img)
 
