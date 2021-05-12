0e9759db99646b3b1b64f181cbc6a83d62724455
Peach8 <btpeterson08@gmail.com>
2021-05-12 11:26:25 -0700

    Layout remaining traj gen procedure

--- trajectory_generation/scripts/robot_trajectory_generation.py
+++ trajectory_generation/scripts/robot_trajectory_generation.py
@@ -6,9 +6,21 @@ import sys
 sys.path.insert(0, './include/')
 from include import workspace_conversions
 
+
+paper_corner_global_pose = Pose2D() # mm coords of paper corner
+
 def callback(data):
-	if data.x != 0.0:
-		rospy.loginfo(workspace_conversions.convert_pixels_to_mm(data.x))
+	if data.x != 0.0: # only take action after SPACEBAR click
+		# - convert paper corner coords from pixels to mm
+		paper_corner_global_pose.x = workspace_conversions.convert_pixels_to_mm(data.x)
+		paper_corner_global_pose.y = workspace_conversions.convert_pixels_to_mm(data.y)
+		# disregard theta for now
+
+		# - offset paper corner to start pos for first number
+
+		# - transform desired number trajectory to mm (TODO: save each offline)
+		#   and apply each to previous offset result
+
 
 def generate_trajectory():
 	rate = rospy.Rate(10) # 10Hz
@@ -23,8 +35,7 @@ def generate_trajectory():
 if __name__ == '__main__':
 	rospy.init_node('robot_trajectory_generation', anonymous=True)
 	rospy.Subscriber('/perception/paper_corner_pixel_coords', Pose2D, callback, queue_size=1)
-	rospy.spin()
-
+	# TODO: add subscriber from mission control for desired numbers to draw
 	# pub = rospy.Publisher
 
 	try:
