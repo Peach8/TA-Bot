9fccbf1cd98eb0c604c994e5c619cef89b0b3206
Peach8 <btpeterson08@gmail.com>
2021-05-11 19:34:48 -0700

    Create trajectory generation package and verify comms with perception package

---
+++ trajectory_generation/scripts/robot_trajectory_generation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import rospy # Python library for ROS
+from geometry_msgs.msg import Pose2D
+import numpy as np
+
+def callback(data):
+	if data.x != 0.0:
+		rospy.loginfo(data.x)
+
+
+def generate_trajectory():
+	rate = rospy.Rate(10) # 10Hz
+
+	while not rospy.is_shutdown():
+		# pub.publish()
+
+		# sleep enough to maintain desired rate
+		rate.sleep()
+
+
+if __name__ == '__main__':
+	rospy.init_node('robot_trajectory_generation', anonymous=True)
+	rospy.Subscriber('/perception/paper_corner_pixel_coords', Pose2D, callback, queue_size=1)
+	rospy.spin()
+
+	# pub = rospy.Publisher
+
+	try:
+		generate_trajectory()	
+	except rospy.ROSInterruptException:
+		pass
