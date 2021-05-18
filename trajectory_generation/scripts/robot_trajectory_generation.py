cd664ccd1fde7079673e24c8bef2b97631a51792
Peach8 <btpeterson08@gmail.com>
2021-05-18 10:41:12 -0700

    Revise naming convention

--- trajectory_generation/scripts/robot_trajectory_generation.py
+++ trajectory_generation/scripts/robot_trajectory_generation.py
@@ -2,24 +2,27 @@
 import rospy # Python library for ROS
 from geometry_msgs.msg import Pose2D
 import numpy as np
-import sys
-sys.path.insert(0, './include/')
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
 from include import workspace_conversions
 
 
-paper_corner_global_pose = Pose2D() # mm coords of paper corner
+paper_fiducial_global_pose = Pose2D() # mm coords of paper corner
+
 
 def callback(data):
 	if data.x != 0.0: # only take action after SPACEBAR click
 		# - convert paper corner coords from pixels to mm
-		paper_corner_global_pose.x = workspace_conversions.convert_pixels_to_mm(data.x)
-		paper_corner_global_pose.y = workspace_conversions.convert_pixels_to_mm(data.y)
+		paper_fiducial_global_pose.x = workspace_conversions.convert_pixels_to_mm(data.x)
+		paper_fiducial_global_pose.y = workspace_conversions.convert_pixels_to_mm(data.y)
 		# disregard theta for now
 
 		# - offset paper corner to start pos for first number
 
-		# - transform desired number trajectory to mm (TODO: save each offline)
-		#   and apply each to previous offset result
+
+		# - add desired number trajectory global coords starting at previous offset
 
 
 def generate_trajectory():
@@ -34,7 +37,7 @@ def generate_trajectory():
 
 if __name__ == '__main__':
 	rospy.init_node('robot_trajectory_generation', anonymous=True)
-	rospy.Subscriber('/perception/paper_corner_pixel_coords', Pose2D, callback, queue_size=1)
+	rospy.Subscriber('/perception/paper_fiducial_pixel_coords', Pose2D, callback, queue_size=1)
 	# TODO: add subscriber from mission control for desired numbers to draw
 	# pub = rospy.Publisher
 
