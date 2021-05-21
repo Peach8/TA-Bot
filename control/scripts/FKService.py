a94bf7c07b9d2fde5db80b874eae7c3a9436eab1
Ophelie2796 <ophelieherve@ucla.edu>
2021-05-20 17:45:42 -0700

    Made ROS specific edits including req.

--- control/scripts/FKService.py
+++ control/scripts/FKService.py
@@ -7,28 +7,27 @@ from control.srv import *
 # TODO: update geometry params
 L1 = 1.0
 L2 = 1.0
-q1 = 0.2
-q2 = 0.3
+
 
 def handle_fk_compute(req):
 	resp = FKComputeResponse()
 
 	# Solve for end of Link 1
-	x1 = L1* math.cos(q1)
-	y1 = L1* math.sin(q1)
+	x1 = L1* math.cos(req.theta1)
+	y1 = L1* math.sin(req.theta1)
 
 	# Solve for end of Link 2
-	resp.x2 = x1 + l2 * math.cos(q2)
-	resp.y2 = y1 + L2 * math.sin(q2)
+	resp.x = x1 + L2 * math.cos(req.theta2)
+	resp.y = y1 + L2 * math.sin(req.theta2)
 
 	return resp
 
 	# Alternatively, if we want the full transformation matrix
-	T = np.array([[ math.cos(q1+q2), -math.sin(q1+q2), L2*math.cos(q1+q2)+L1*cos(q1)],
-		[math.sin(q1+q2), math.cos(q1+q2), L2*math.sin(q1+q2)+L1*math.sin(q1)],
-		[0, 0, 0, 1]])
-	#resp.xend = T[0][2]
-	#resp.yend = T[1][2]
+	#T = np.array([[ math.cos(req.theta1+req.theta2), -math.sin(req.theta1+req.theta2), L2*math.cos(req.theta1+req.theta2)+L1*cos(req.theta1)],
+	#	[math.sin(req.theta1+req.theta2), math.cos(req.theta1+req.theta2), L2*math.sin(req.theta1+req.theta2)+L1*math.sin(req.theta1)],
+	#	[0, 0, 0, 1]])
+	#resp.x = T[0][2]
+	#resp.y = T[1][2]
 	#return resp
 
 if __name__ == "__ophelie_fk":
